a2bbf6c6f088f6e065541816365273478662eca6
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2019-03-09 10:39:40 +0200

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
+
+
+import ArtistsScreen from './artists/artists.screen.js'
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-           Hello111
-          </a>
-        </header>
+      <div>
+       home
+        <br/>
+       <Router>
+         <Route path="/artists" component={ArtistsScreen} />
+       </Router>
+        
       </div>
     );
   }
---
+++ src/artists/artists.screen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Link,
+} from 'react-router-dom'
+
+class ArtistsScreen extends Component {
+  render() {
+    return (
+      <div>
+      <Link to='/'>Go home</Link>
+        ArtistsScreen2
+      
+        
+      </div>
+    );
+  }
+}
+
+export default ArtistsScreen;

f3b977259b53ecbbc5686debd10a95ba70475f51
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2019-04-14 19:36:03 +0300

    build

---
+++ build/precache-manifest.f68ddc2124c872692afb32758269b943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "224f2b274cedc250faf6",
+    "url": "/static/js/main.224f2b27.chunk.js"
+  },
+  {
+    "revision": "6e11ddf757719d8e2d9c",
+    "url": "/static/js/1.6e11ddf7.chunk.js"
+  },
+  {
+    "revision": "224f2b274cedc250faf6",
+    "url": "/static/css/main.2ec6c350.chunk.css"
+  },
+  {
+    "revision": "1db4c9beaa097b3a5532e471bad3b8fa",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Shperung/significanceCanto/precache-manifest.6cf1f946889d9a27b9212d177dfda2f4.js"
+  "/precache-manifest.f68ddc2124c872692afb32758269b943.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Shperung/significanceCanto/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

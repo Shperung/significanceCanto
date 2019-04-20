cafe2aef18449442f6769561ba5343bfe00969fe
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2019-04-20 13:12:18 +0300

    add routs api

--- serverApp.js
+++ serverApp.js
@@ -10,7 +10,38 @@ let db;
 
 const port = process.env.PORT || 3012;
 
+const artists = [
+	{
+        id: 1,
+        unique: 'nightwish2',
+		name: "Nightwish2"
+	},
+	{
+        id: 2,
+        unique: 'rammstein2',
+		name: "Rammstein2"
+	},
+	{
+        id: 3,
+        unique: 'within-temptation2',
+		name: "Within Temptation2"
+	},
+];
 
+app.get('/api/artists', (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+	res.send(artists);
+});
+
+app.get('/api/artists/:unique', (req, res) => {
+	const artist = artists.find(artist => artist.unique === req.params.unique);
+	if (artist) {
+		res.send(artist);
+	} else {
+		res.send(`Artist with unique - ${req.params.unique} no found`);
+	}
+});
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello API3</h1>');

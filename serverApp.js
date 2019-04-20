738e77148ee1d62aa9277215ac50f01660451028
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2019-04-20 14:13:52 +0300

    update server

--- serverApp.js
+++ serverApp.js
@@ -49,8 +49,10 @@ app.get('/api/artists/:unique', (req, res) => {
 	}
 });
 
+app.use(express.static(path.resolve(__dirname, 'build')));
+
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 

ea05d1cb7270275fb8f1fd74ef562a2a15bf0788
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2021-01-17 14:46:39 +0200

    build 4

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout/layout.jsx";
 const Index = () => (
   <Layout>
     <h1>Main page</h1>
-    <div>hext index page55 test 1</div>
+    <div>hext index page55 test 3</div>
   </Layout>
 );
 
